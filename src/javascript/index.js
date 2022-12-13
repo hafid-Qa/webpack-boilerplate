d27f622722841f7dc848ce26c6d9ea9b66cb15fe
Hafid Qarchi <haqarchi@gmail.com>
2022-12-13 17:36:51 +0900

    style for home page

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,7 @@
 import "../stylesheets/style.scss";
+import emoji from "../images/emoji.png";
+
+const img = document.querySelector("#img");
+img.src = emoji;
 
 console.log("Hello from src/javascript/index.js!");
